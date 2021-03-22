43de7e9ce558f0449a861835d5a7177a4e4503cc
Pranit <75229720+pranitb27@users.noreply.github.com>
2021-03-22 20:19:01 +0530

    at 22-03-2021 20:18

--- 02_Basics/marvels.js
+++ 02_Basics/marvels.js
@@ -1,10 +1,32 @@
-const Superheros = ['Superman','Captain Ameria','Batman','Ironman'];
+const Superheroes = ['Superman','Captain Ameria','Batman','Ironman'];
 
-console.log(Superheros);
-console.log(Superheros[Superheros.length-1]);
+console.log(Superheroes);
+console.log(Superheroes.valueOf());
 
-
-for(let i=0;i<Superheros.length;i++)
+for(let i=0;i<Superheroes.length;i++)
 {
-  console.log(Superheros[i]);
+  if(Superheroes[i] == 'Batman')
+    {console.log(Superheroes[i]);
+    Superheroes[i]='Thor'; }
 }
+console.log(Superheroes);
+console.log(`We have ${Superheroes.length} Superheroes on our side`);
+
+console.log('Shifted : '+Superheroes.shift());
+printer();
+console.log('Unshifted : '+Superheroes.unshift('Shaktiman'));
+printer();
+console.log('Popped : '+ Superheroes.pop());
+printer();
+console.log('Pushed : '+ Superheroes.push('Saitama'));
+console.log('Splicing : '+Superheroes.splice(1,2,'Luffy','Zoro'));
+printer();
+
+function printer() {
+  var a=Superheroes.entries();
+  for(x of a)
+  console.log(x);
+}
+console.log("\nNinja way is here\n");
+Superheroes[Superheroes.length]= "Naruto";
+printer();
