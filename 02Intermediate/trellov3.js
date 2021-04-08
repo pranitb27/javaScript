447d2b9a7ce9a84d548de1783363de8da063e229
Pranit <75229720+pranitb27@users.noreply.github.com>
2021-04-08 20:24:45 +0530

    08-04-2021 Using arrow functions

--- 02Intermediate/trellov3.js
+++ 02Intermediate/trellov3.js
@@ -20,4 +20,16 @@ var findCarStatus = function(mycars,input)
    return mycars[index].status;
 };
 
-console.log(findCarStatus(cars,'Fiat'));
+//console.log(findCarStatus(cars,'Fiat'));
+//Normal function
+var findCarName = (cars, input) => {
+  let data = cars.filter(function(car){ return car.status === input});
+  if (data.length > 0){
+    data.forEach((item, i) => console.log(item.title));
+}
+}
+findCarName(cars,'running');
+
+//Arrow function
+var findCarDetail = (cars, input) => cars.filter((car) => car.status === input)
+//console.log(findCarDetail(cars,'running'));
