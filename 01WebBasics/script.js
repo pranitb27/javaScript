e67c4e58bb735d343d7b95b340de026e13a91d75
Pranit <75229720+pranitb27@users.noreply.github.com>
2021-04-11 22:13:06 +0530

    11-04-2021 Web basics

---
+++ 01WebBasics/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementById("idone").innerHTML);
+console.log(document.getElementsByClassName("classone")[0].innerText);
+
+console.log(document.querySelector(".classone").innerText);
+console.log(document.querySelectorAll("p")[3].innerHTML);
